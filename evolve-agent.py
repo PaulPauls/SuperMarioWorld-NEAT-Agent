e47a10b0e0d6134756c71c2e80da50140d333b57
Paul Pauls <mail@paulpauls.de>
2019-05-03 15:29:38 +0200

    Integrate functionality of parallel trainng into evolve-agent.py and control it via flag

--- evolve-agent.py
+++ evolve-agent.py
@@ -12,6 +12,10 @@ import cv2
 env = retro.make('SuperMarioWorld-Snes', 'YoshiIsland1')
 max_generations = 300
 cv_resolution_divisor = 8
+nr_parallel_env = 4
+
+# Global variable to keep program simple
+env_reshape_tuple = ()
 
 
 def reward_from_data(data):
@@ -37,44 +41,46 @@ def end_genome_train_condition(data, time_to_xpos_dict):
     return False
 
 
+def eval_genome(genome, config):
+    """ Train a single on the environment by letting it convert the computer vision input to button outputs
+        and the evaluate and return (not save because parallel env!) its fitness according to reward_form_data """
+    nn = neat.nn.recurrent.RecurrentNetwork.create(genome, config)
+    time_to_xpos_dict = {}
+    fitness = 0
+    done = False
+
+    # cv2.namedWindow("machine_view", cv2.WINDOW_NORMAL)
+    input_prepoc = env.reset()
+
+    while not done:
+        env.render()
+
+        input_prepoc = cv2.resize(input_prepoc, env_reshape_tuple)
+        input_prepoc = cv2.cvtColor(input_prepoc, cv2.COLOR_BGR2GRAY)
+        nn_input = np.ndarray.flatten(input_prepoc)
+
+        # cv2.imshow("machine_view", input_prepoc)
+        # cv2.waitKey(1)
+
+        nn_output = nn.activate(nn_input)
+
+        input_prepoc, reward, done, data = env.step(nn_output)
+
+        if done or end_genome_train_condition(data, time_to_xpos_dict) or data['lives'] == 3:
+            done = True
+            fitness = reward_from_data(data)
+
+    return fitness
+
+
 def eval_genomes(genomes, config):
-    """ Train each genome on the environment by letting it convert the computer vision input to button outputs
-        and the evaluate and save its fitness according to reward_form_data """
-    in_x, in_y, _ = env.observation_space.shape
-    env_reshape_tuple = (int(in_x / cv_resolution_divisor), int(in_y / cv_resolution_divisor))
-
+    """ Perform eval_genome on each genome """
     for genome_id, genome in genomes:
-        nn = neat.nn.recurrent.RecurrentNetwork.create(genome, config)
-        time_to_xpos_dict = {}
-        genome.fitness = 0
-        done = False
-
-        # cv2.namedWindow("machine_view", cv2.WINDOW_NORMAL)
-        input_prepoc = env.reset()
-
-        while not done:
-            env.render()
-
-            input_prepoc = cv2.resize(input_prepoc, env_reshape_tuple)
-            input_prepoc = cv2.cvtColor(input_prepoc, cv2.COLOR_BGR2GRAY)
-            nn_input = np.ndarray.flatten(input_prepoc)
-
-            # cv2.imshow("machine_view", input_prepoc)
-            # cv2.waitKey(1)
-
-            nn_output = nn.activate(nn_input)
-
-            input_prepoc, reward, done, data = env.step(nn_output)
-
-            # genome.fitness += reward
-
-            if done or end_genome_train_condition(data, time_to_xpos_dict) or data['lives'] == 3:
-                done = True
-                genome.fitness = reward_from_data(data)
-                print("Genome_ID: {} \t\tFitness: {}".format(genome_id, genome.fitness))
+        genome.fitness = eval_genome(genome, config)
+        print("Genome_ID: {} \t\tFitness: {}".format(genome_id, genome.fitness))
 
 
-def run(config_file_path, checkpoint_file_path=None):
+def run(config_file_path, checkpoint_file_path=None, parallel=False):
     """ Load Neat configuration with all Defaults, register reporters and train the population with eval_genomes """
     config = neat.Config(neat.DefaultGenome,
                          neat.DefaultReproduction,
@@ -95,8 +101,17 @@ def run(config_file_path, checkpoint_file_path=None):
         print("Using pre-existing checkpoint and configuration! Possible different config file is dismissed!")
         pop = neat.Checkpointer.restore_checkpoint(checkpoint_file_path)
 
-    # Run for up to a specified max number of generations.
-    best_genome = pop.run(eval_genomes, max_generations)
+    # Calculate reshape resolution tuple according to specified parameters
+    in_x, in_y, _ = env.observation_space.shape
+    global env_reshape_tuple
+    env_reshape_tuple = (int(in_x / cv_resolution_divisor), int(in_y / cv_resolution_divisor))
+
+    # Run either paralle or single-threaded for specified max number of generations.
+    if parallel:
+        pe = neat.ParallelEvaluator(nr_parallel_env, eval_genome)
+        best_genome = pop.run(pe.evaluate, max_generations)
+    else:
+        best_genome = pop.run(eval_genomes, max_generations)
 
     # Display and save the winning genome.
     print('\nBest genome:\n{!s}'.format(best_genome))
